259bac7903ba9e4da5af35d1bfcaba9a8aa78b47
willy <willyanalvesgomes@hotmail.com>
2026-09-17 17:09:41 -0300

    Alterando caminho para usar o main

--- main.py
+++ main.py
@@ -11,13 +11,13 @@ while True:
     opcao = input("Escolha uma opção: ")
 
     if opcao == "1":
-        subprocess.run(["python", "Parte2_Ordenacao/parte2.py"])
+        subprocess.run(["python", "Atividade_2/Parte2_Ordenacao/parte2.py"])
     elif opcao == "2":
-        subprocess.run(["python", "Parte3_BuscaMatriz/parte3.py"])
+        subprocess.run(["python", "Atividade_2/Parte3_BuscaMatriz/parte3.py"])
     elif opcao == "3":
-        subprocess.run(["python", "Parte4_HandsOn1/parte4.py"])
+        subprocess.run(["python", "Atividade_2/Parte4_HandsOn1/parte4.py"])
     elif opcao == "4":
-        subprocess.run(["python", "Parte5_HandsOn2/parte5.py"])
+        subprocess.run(["python", "Atividade_2/Parte5_HandsOn2/parte5.py"])
     elif opcao == "0":
         print("Encerrando...")
         break
